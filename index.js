b202821fe7351d1f464e9ea48ee8433fd81116e5
changwoolab <cwyoo01@naver.com>
2024-03-27 01:07:44 +0900

    Revert "feat: adds default value for swc transformer"
    This reverts commit 630f50ea22f4075d1820d31e701fbdf4226eb572.

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
 const flowRemoveTypes = require("flow-remove-types");
 const createSwcTransformer = require("@swc/jest").createTransformer;
 
-const swcTransformer = createSwcTransformer({
-  jsc: {
-    parser: {
-      syntax: "typescript",
-      jsx: true,
-      tsx: true,
-      dynamicImport: false,
-      privateMethod: false,
-      functionBind: false,
-      exportDefaultFrom: false,
-      exportNamespaceFrom: false,
-      decorators: false,
-      decoratorsBeforeExport: false,
-      topLevelAwait: false,
-      importMeta: false,
-      preserveAllComments: false,
-    },
-    transform: null,
-    target: "es5",
-    loose: true,
-    externalHelpers: false,
-    // Requires v1.2.50 or upper and requires target to be es2016 or upper.
-    keepClassNames: false,
-  },
-});
+const swcTransformer = createSwcTransformer();
 
 module.exports = {
   process(src, filename, jestOptions) {
